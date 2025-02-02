3c71f8fbd13aa8048bc00cef1ad8af1b0ffdde2c
karthick19012025 <realorfakereview@gmail.com>
2025-02-02 17:42:47 +0530

    web app first commit 2

--- src/main/java/com/maths/app/CalculatorApplication.java
+++ src/main/java/com/maths/app/CalculatorApplication.java
@@ -3,14 +3,16 @@ package com.maths.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CalculatorApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(CalculatorApplication.class);
     }
